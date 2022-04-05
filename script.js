de77054fa0f0a5efd2038b4bb67d59b0801ebb4c
Penna92 <giacomopenna92@gmail.com>
2022-04-05 21:47:41 +0200

    Ultimata funzione che gestisce la fine del gioco : aggiunto bonus 2.

--- script.js
+++ script.js
@@ -32,112 +32,116 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 
 // FUNZIONE CHE GENERA NUMERI RANDOM
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+const BOMB_NUMBER = 16;
+const bombs = [];
+let max_attempt;
+let attempts = 0;
+
+//FUNZIONE CHE FA SCEGLIERE IL LIVELLO ALL'UTENTE
+function setLevel(event) {
+  //console.log(this);
+  //console.log(event.target);
+  //event.preventDefault();
+  const level = document.getElementById("level").value;
+  console.log("livello selezionato: ", level);
+  let numSquare;
+  switch (level) {
+    case "1":
+    default:
+      numSquare = 100;
+      break;
+    case "2":
+      numSquare = 81;
+      break;
+    case "3":
+      numSquare = 49;
+      break;
   }
-  const BOMB_NUMBER = 16;
-  const bombs = [];
-  let max_attempt;
-  let attempts = 0;
-  
-  //FUNZIONE CHE FA SCEGLIERE IL LIVELLO ALL'UTENTE
-  function setLevel(event) {
-    //console.log(this);
-    //console.log(event.target);
-    //event.preventDefault();
-    const level = document.getElementById("level").value;
-    console.log("livello selezionato: ", level);
-    let numSquare;
-    switch (level) {
-      case "1":
-      default:
-        numSquare = 100;
-        break;
-      case "2":
-        numSquare = 81;
-        break;
-      case "3":
-        numSquare = 49;
-        break;
+  let squareperSide = Math.sqrt(numSquare);
+  console.log("celle per lato: ", squareperSide);
+  generateBomb(numSquare);
+  generaGriglia(numSquare, squareperSide);
+}
+
+//FUNZIONE CHE GENERA LE BOMBE
+function generateBomb(numSquare) {
+  max_attempt = numSquare - BOMB_NUMBER;
+  while (bombs.length < BOMB_NUMBER) {
+    let bombNUmber = getRandomInt(1, numSquare);
+    if (!bombs.includes(bombNUmber)) {
+      bombs.push(bombNUmber);
     }
-    let squareperSide = Math.sqrt(numSquare);
-    console.log("celle per lato: ", squareperSide);
-    generateBomb(numSquare);
-    generaGriglia(numSquare, squareperSide);
   }
-  
-  //FUNZIONE CHE GENERA LE BOMBE
-  function generateBomb(numSquare) {
-    max_attempt = numSquare - BOMB_NUMBER;
-    while (bombs.length < BOMB_NUMBER) {
-      let bombNUmber = getRandomInt(1, numSquare);
-      if (!bombs.includes(bombNUmber)) {
-        bombs.push(bombNUmber);
+}
+
+// FUNZIONE CHE GENERA LA GRIGLIA
+function generaGriglia(numSquare, squareperSide) {
+  console.log("numero di celle totali: ", numSquare);
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+  let row = document.createElement("div");
+  row.setAttribute("class", "gridrow");
+  for (let i = 1; i <= numSquare; i++) {
+    const square = generaCella(i, squareperSide);
+    row.append(square);
+  }
+  app.append(row);
+}
+
+// FUNZIONE CHE GENERA LE CELLE
+function generaCella(numSquare, squareperSide) {
+  let square = document.createElement("div");
+  square.setAttribute("class", "box");
+  square.style.width = `calc(100% / ${squareperSide})`;
+  square.style.height = `calc(100% / ${squareperSide})`;
+  square.classList.add("pointer");
+  square.innerHTML = `<span>${numSquare}</span>`;
+  square.addEventListener("click", coloraCella);
+  return square;
+}
+
+// FUNZIONE CHE COLORA LE CELLE AL CLICK
+function coloraCella() {
+  // console.log(this.innerText);
+  let num = parseInt(this.innerText);
+  attempts++;
+
+  if (bombs.includes(num)) {
+    this.style.backgroundColor = "red";
+    this.innerHTML = `<img src="img/bomb.png">`;
+    gameOver();
+  } else {
+    this.style.backgroundColor = "#6495ed";
+    this.style.color = "white";
+  }
+  this.classList.remove("pointer");
+  this.removeEventListener("click", coloraCella);
+}
+// FUNZIONE CHE GESTISCE LA FINE DEL GIOCO
+function gameOver() {
+  if (attempts == max_attempt) {
+    alert("Complimenti, hai vinto! Non hai clickato su nessuna bomba!");
+  } else {
+    alert("Hai totalizzato un punteggio di : " + parseInt(attempts - 1));
+  }
+  let caselleRestanti = document.getElementsByClassName("box");
+
+  for (let i = 0; i < caselleRestanti.length; i++) {
+    let rimuoviEvento = caselleRestanti[i];
+    rimuoviEvento.classList.remove("pointer");
+    rimuoviEvento.removeEventListener("click", coloraCella);
+    for (let n = 0; n < bombs.length; n++) {
+      if (bombs[n] == rimuoviEvento.innerText) {
+        console.log(bombs[n], rimuoviEvento.innerText);
+        rimuoviEvento.style.backgroundColor = "red";
+        rimuoviEvento.innerHTML = `<img src="img/bomb.png">`;
       }
     }
   }
-  
-  // FUNZIONE CHE GENERA LA GRIGLIA
-  function generaGriglia(numSquare, squareperSide) {
-    console.log("numero di celle totali: ", numSquare);
-    const app = document.getElementById("app");
-    app.innerHTML = "";
-    let row = document.createElement("div");
-    row.setAttribute("class", "gridrow");
-    for (let i = 1; i <= numSquare; i++) {
-      const square = generaCella(i, squareperSide);
-      row.append(square);
-    }
-    app.append(row);
-  }
-  
-  // FUNZIONE CHE GENERA LE CELLE
-  function generaCella(numSquare, squareperSide) {
-    let square = document.createElement("div");
-    square.setAttribute("class", "box");
-    square.style.width = `calc(100% / ${squareperSide})`;
-    square.style.height = `calc(100% / ${squareperSide})`;
-    square.classList.add("pointer");
-    square.innerHTML = `<span>${numSquare}</span>`;
-    square.addEventListener("click", coloraCella);
-    return square;
-  }
-  
-  // FUNZIONE CHE COLORA LE CELLE AL CLICK
-  function coloraCella() {
-    //console.log(this.innerText);
-    let num = parseInt(this.innerText);
-    attempts++;
-    if (bombs.includes(num)) {
-      this.style.backgroundColor = "red";
-      this.innerHTML = `<img src="img/bomb.png">`;
-      gameOver();
-    } else {
-      this.style.backgroundColor = "#6495ed";
-      this.style.color = "white";
-    }
-    this.classList.remove("pointer");
-    this.removeEventListener("click", coloraCella);
-  }
-  
-  // FUNZIONE CHE GESTISCE LA FINE DEL GIOCO
-  function gameOver() {
-    if (attempts == max_attempt) {
-      alert("Complimenti, hai vinto! Non hai clickato su nessuna bomba!");
-    } else {
-      alert("Hai clickato su una bomba al tentativo numero : " + attempts);
-    }
-    let caselleRestanti = document.getElementsByClassName("pointer");
-  
-    for (let i = 0; i < caselleRestanti.length; i++) {
-      let rimuoviEvento = caselleRestanti[i];
-      rimuoviEvento.classList.remove("pointer");
-      rimuoviEvento.removeEventListener("click", coloraCella);
-      console.log(rimuoviEvento);
-      i--;
-    }
-  }
-  //document.getElementById("level").addEventListener("change", setLevel);
-  document.getElementById("play").addEventListener("click", setLevel);
-  
+}
+//document.getElementById("level").addEventListener("change", setLevel);
+document.getElementById("play").addEventListener("click", setLevel);
